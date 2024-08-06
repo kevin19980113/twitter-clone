cd91657b3284f9f68abdfb6abdb17a7dd16d0f66
HYUNHO <jeny95kr@gmail.com>
2024-08-06 15:53:43 -0400

    changed authStore

--- frontend/src/hooks/use-store.ts
+++ frontend/src/hooks/use-store.ts
@@ -1,53 +1,11 @@
-import { StateCreator, create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
-import { immer } from "zustand/middleware/immer";
-import { User } from "../types/userType";
-
-type storeState = {
-  user: User | null;
-};
-
-type storeActions = {
-  setUser: (user: User | null) => void;
-};
+import { create } from "zustand";
 
 type AuthStore = {
   accessToken: string | null;
   setAccessToken: (accessToken: string | null) => void;
 };
 
-type storeSlice = storeState & storeActions;
-
-const createStoreSlice: StateCreator<
-  storeSlice,
-  [["zustand/immer", never], ["zustand/subscribeWithSelector", never]],
-  [],
-  storeSlice
-> = (set) => ({
-  user: null,
-  setUser: (user) => set(() => ({ user })),
-});
-
-export const useStore = create<storeSlice>()(
-  subscribeWithSelector(
-    immer((...a) => ({
-      ...createStoreSlice(...a),
-    }))
-  )
-);
-
 export const useAuthStore = create<AuthStore>()((set) => ({
   accessToken: null,
   setAccessToken: (token) => set({ accessToken: token }),
 }));
-
-// useStore.subscribe(
-//   (state) => state.user,
-//   (user) => {
-//     useStore.getState().setUser(user);
-//   },
-//   {
-//     equalityFn: shallow,
-//     fireImmediately: true,
-//   }
-// );
